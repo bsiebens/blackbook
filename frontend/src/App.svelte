e5df195d419af2a43edd3b528aa15e180cc0a10d
Bernard Siebens <bernard@siebens.org>
2020-12-16 17:06:12 +0100

    Some first attempt at creating a login store

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,11 +1,8 @@
 <script>
+	import Login from "./components/Login.svelte";
+
 	export let name;
 </script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
 <style>
 	main {
@@ -28,3 +25,14 @@
 		}
 	}
 </style>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>
+		Visit the
+		<a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+		to learn how to build Svelte apps.
+	</p>
+
+	<Login />
+</main>
---
+++ frontend/src/components/Login.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { store } from "../stores/auth";
+
+    let username = "";
+    let password = "";
+
+    function handleSubmit() {
+        $store = { username };
+    }
+</script>
+
+<div>
+    <h1>Sign In</h1>
+    <pre>{JSON.stringify($store, null, 2)}</pre>
+
+    <form on:submit|preventDefault={handleSubmit}>
+        <label> Username: </label>
+        <input type="text" bind:value={username} placeholder="Username" />
+
+        <label> Password: </label>
+        <input type="password" bind:value={password} />
+
+        <button type="submit">Submit</button>
+    </form>
+</div>
