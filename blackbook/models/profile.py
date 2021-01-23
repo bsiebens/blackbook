6b374da84d16e1b7c134b4288fa36d52c9093cc0
Bernard Siebens <bernard@siebens.org>
2021-01-23 21:08:58 +0100

    Updated relationship between UserProfile and User from ForeignKey to OneToOne

--- blackbook/models/profile.py
+++ blackbook/models/profile.py
@@ -7,7 +7,7 @@ from .base import get_default_currency, get_currency_choices
 
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     default_currency = CurrencyField(default=get_default_currency(), choices=get_currency_choices())
 
     created = models.DateTimeField(auto_now_add=True)
