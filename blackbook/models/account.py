cd905d43f7618dab3c8034c1133685335abf0aa0
Bernard Siebens <bernard@siebens.org>
2021-01-31 09:39:16 +0100

    Only filter those transactions with an amount that has the same currency as the account

--- blackbook/models/account.py
+++ blackbook/models/account.py
@@ -74,7 +74,12 @@ class Account(models.Model):
 
     def balance_until_date(self, date=timezone.localdate()):
         try:
-            total = Money(self.transactions.filter(transaction__date__lte=date).aggregate(total=Coalesce(Sum("amount"), 0))["total"], self.currency)
+            total = Money(
+                self.transactions.filter(transaction__date__lte=date)
+                .filter(transaction__amount_currency=self.currency)
+                .aggregate(total=Coalesce(Sum("amount"), 0))["total"],
+                self.currency,
+            )
 
             return total - Money(self.virtual_balance, self.currency)
 
