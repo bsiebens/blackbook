0d87c632cb3091c55bf1f1d03a96ff9c4bb27b7f
Bernard Siebens <bernard@siebens.org>
2021-05-07 19:13:15 +0200

    Started work on the paycheck module

--- blackbook/models/__init__.py
+++ blackbook/models/__init__.py
@@ -5,3 +5,4 @@ from .profile import UserProfile
 from .transaction import TransactionJournal, Transaction
 from .category import Category
 from .budget import Budget, BudgetPeriod
+from .paycheck import Paycheck, PayCheckItem, PayCheckItemCategory, Bonus
